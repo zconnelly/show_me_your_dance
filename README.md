7884b581de5f491893ed024b48f235c4868d7737
Zac Connelly <zac@mimi.io>
2018-04-11 09:51:50 +0200

    okay we really going now

---
+++ README.md
@@ -0,0 +1,84 @@
+"""
+DDR Vim
+=======
+
+### Find the mat!
+
+```python
+#!/usr/bin/python
+import sys
+import usb.core
+# find USB devices
+dev = usb.core.find(find_all=True)
+# loop through devices, printing vendor and product ids in decimal and hex
+for cfg in dev:
+  sys.stdout.write(
+    'Decimal VendorID=' + str(cfg.idVendor) +
+    ' & ProductID=' + str(cfg.idProduct) + '\n')
+  sys.stdout.write(
+    'Hexadecimal VendorID=' + hex(cfg.idVendor) +
+    ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
+```
+
+### List USB Devices
+
+```bash
+# list usb devices
+ioreg -p IOUSB -w0
+```
+
+### The Dance Mat Info
+
+```text
+# the mat
+Decimal VendorID=26231 & ProductID=34833
+Hexadecimal VendorID=0x6677 & ProductID=0x8811
+```
+
+### The Dance Mat Data
+
+```
+# codes
+up
+[4, 0, 128, 0]
+[0, 0, 128, 128]
+right
+[8, 0, 255, 128]
+[0, 0, 128, 128]
+left
+[1, 0, 0, 128]
+[0, 0, 128, 128]
+down
+[2, 0, 128, 255]
+[0, 0, 128, 128]
+up right
+[128, 0, 128, 128]
+[0, 0, 128, 128]
+[128, 0, 128, 128]
+[0, 0, 128, 128]
+down right
+[32, 0, 128, 128]
+[0, 0, 128, 128]
+down left
+[16, 0, 128, 128]
+[0, 0, 128, 128]
+[4, 0, 128, 0]
+[0, 0, 128, 128]
+[16, 0, 128, 128]
+[0, 0, 128, 128]
+up left
+[64, 0, 128, 128]
+[0, 0, 128, 128]
+select
+[0, 2, 128, 128]
+[0, 0, 128, 128]
+start[0, 1, 128, 128]
+[0, 0, 128, 128]
+```
+
+### Sending Keys
+
+```bash
+osascript -e 'tell application "System Events" to keystroke "m"'
+```
+"""
--- main.py
+++ main.py
@@ -1,117 +1,67 @@
-"""
-DDR Vim
-=======
-
-Find the mat!
-
-```python
-#!/usr/bin/python
-import sys
-import usb.core
-# find USB devices
-dev = usb.core.find(find_all=True)
-# loop through devices, printing vendor and product ids in decimal and hex
-for cfg in dev:
-  sys.stdout.write(
-    'Decimal VendorID=' + str(cfg.idVendor) +
-    ' & ProductID=' + str(cfg.idProduct) + '\n')
-  sys.stdout.write(
-    'Hexadecimal VendorID=' + hex(cfg.idVendor) +
-    ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
-```
-
-```bash
-# list usb devices
-ioreg -p IOUSB -w0
-```
-
-```text
-# the mat
-Decimal VendorID=26231 & ProductID=34833
-Hexadecimal VendorID=0x6677 & ProductID=0x8811
-```
-
-```
-# codes
-up
-[4, 0, 128, 0]
-[0, 0, 128, 128]
-right
-[8, 0, 255, 128]
-[0, 0, 128, 128]
-left
-[1, 0, 0, 128]
-[0, 0, 128, 128]
-down
-[2, 0, 128, 255]
-[0, 0, 128, 128]
-up right
-[128, 0, 128, 128]
-[0, 0, 128, 128]
-[128, 0, 128, 128]
-[0, 0, 128, 128]
-down right
-[32, 0, 128, 128]
-[0, 0, 128, 128]
-down left
-[16, 0, 128, 128]
-[0, 0, 128, 128]
-[4, 0, 128, 0]
-[0, 0, 128, 128]
-[16, 0, 128, 128]
-[0, 0, 128, 128]
-up left
-[64, 0, 128, 128]
-[0, 0, 128, 128]
-select
-[0, 2, 128, 128]
-[0, 0, 128, 128]
-start[0, 1, 128, 128]
-[0, 0, 128, 128]
-```
-"""
-from __future__ import print_function
-
 import hid
+import os
 import time
 
-# try opening a device, then perform write and read
+buttons = {
+    'up': [4, 0, 128, 0],
+    'right': [8, 0, 255, 128],
+    'left': [1, 0, 0, 128],
+    'down': [2, 0, 128, 255],
+    'up_right': [128, 0, 128, 128],
+    'down_right': [32, 0, 128, 128],
+    'down_left': [16, 0, 128, 128],
+    'up_left': [64, 0, 128, 128],
+    'select': [0, 2, 128, 128],
+    'start': [0, 1, 128, 128],
+    'release': [0, 0, 128, 128],
+}
 
-try:
-    print("Opening the device")
+button_lookup = {tuple(value): key for key, value in buttons.iteritems()}
 
+dance_map = {
+    'right': 'm',
+}
+
+
+def send_keys(keys, options=""):
+    cmd = """
+    osascript -e 'tell application "System Events" to keystroke "%s" %s'
+    """ % (keys, options)
+    os.system(cmd)
+
+
+def start_device():
+    print("Initializing Dance Mat")
     h = hid.device()
-    h.open(0x6677, 0x8811)  # TREZOR VendorID/ProductID
-
-    print("Manufacturer: %s" % h.get_manufacturer_string())
-    print("Product: %s" % h.get_product_string())
-    print("Serial No: %s" % h.get_serial_number_string())
+    h.open(0x6677, 0x8811)
 
     # enable non-blocking mode
     h.set_nonblocking(1)
 
     # write some data to the device
-    print("Write the data")
     h.write([0, 63, 35, 35] + [0] * 61)
 
     # wait
     time.sleep(0.10)
+    return h
 
-    # read back the answer
-    print("Read the data")
+
+def main(device):
+    print("Reading data in")
     while True:
-        d = h.read(64)
-        if d:
-            print(d)
+        data = device.read(64)
+        if data:
+            button = button_lookup[tuple(data)]
+            print(button)
+            if button in dance_map:
+                send_keys(dance_map[button])
         else:
             time.sleep(0.05)
 
-    print("Closing the device")
-    h.close()
 
+try:
+    device = start_device()
+    main(device)
 except IOError as ex:
     print(ex)
-    print("You probably don't have the hard coded device. Update the hid.device line")
-    print("in this script with one from the enumeration list output above and try again.")
-
-print("Done")
+    print("You probably don't have the hard coded device.")
